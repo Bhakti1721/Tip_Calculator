d091a5edbd020d89486b913bfa0a4740a2da15bd
Bhakti1721 <149001747+Bhakti1721@users.noreply.github.com>
2023-11-14 12:46:23 -0600

    Add files via upload

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -2,5 +2,6 @@ print("Welcome to Tip Calculator")
 people = int(input("Enter the number of people :\n"))
 bill = float(input("Enter the bill amount :\n"))
 tip = int(input("Tip to add in bill :\n"))
-sum =(bill/people)*tip
+bill_with_tip = tip/100 * bill + bill
+sum = bill_with_tip/people
 print(f"Each Person should Pay :{sum}")
